404f8d9025fa535c0445ec7a15022a458b338da6
Himesh Panchal <himesh.panchal@akuity.io>
2026-07-01 15:23:02 +0530

    feat(tf): tenant TF roots (canary/prod) calling podinfo module @ v6.13.0

---
+++ tf/tenants/canary/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = "https://kubernetes.default.svc"
+  token                  = file("/var/run/secrets/kubernetes.io/serviceaccount/token")
+  cluster_ca_certificate = file("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
+}
+
+# The module ref IS the version pin. Kargo's hcl-update bumps ?ref=... here.
+module "podinfo" {
+  source    = "git::https://github.com/himeshpanc/tf-podinfo-module.git//?ref=v6.13.0"
+  namespace = "tf-canary"
+}
