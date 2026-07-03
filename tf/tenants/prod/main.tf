72de2ce95d5c1ae4d947ef37d4073c0b09078325
Kargo <no-reply@kargo.io>
2026-07-03 05:11:32 +0000

    promote podinfo module 6.20.0 to tf/tenants/prod (prod)

--- tf/tenants/prod/main.tf
+++ tf/tenants/prod/main.tf
@@ -22,7 +22,7 @@ provider "kubernetes" {
 
 # The module ref IS the version pin. Kargo's hcl-update bumps ?ref=... here.
 module "podinfo" {
-  source    = "git::https://github.com/himeshpanc/tf-podinfo-module.git//?ref=6.19.0"
+  source    = "git::https://github.com/himeshpanc/tf-podinfo-module.git//?ref=6.20.0"
   namespace = "tf-prod"
   replicas  = 2
 }
